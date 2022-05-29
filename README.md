03e425eff41e843747ed686711f8553d6c2e448f
Santhoshkumard11 <santhoshkdhana@gmail.com>
2022-05-29 23:12:16 +0530

    udpated tests and README.md

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
-# power-pay-with-Rapyd
-Integrating Payment into Power Apps
+# Power Pay with Rapyd
+B2B payments are done smart, fast, and in an extra secure way with Microsoft's Power Platform tools
+
+### NOTE: Please check the Judge section for email and password to access the Microsoft office tenant used to build this application and steps to test the application
+
+## YouTube Demo Video 📺📺
+### [Click here to watch the demo](https://www.youtube.com/watch?v=WWD2JrgJoss&ab_channel=LateNightCodewithSanthosh)
+
+
+<a href="https://youtu.be/WWD2JrgJoss">
+  <img src="https://img.youtube.com/vi/WWD2JrgJoss/hqdefault.jpg" alt="video">
+</a>
+
+## Setup environment variable
+#### Please set the below variables
+- RAPYD_ACCESS_KEY - Rapyd sandbox access key
+- RAPYD_SECRET_KEY - Rapyd sandbox secret key
+- MSFT_CLIENT_ID - Microsoft Azure Active Directory Client ID
+- MSFT_CLIENT_SECRET - Microsoft Azure Active Directory Client Secret
+- TENANT_ID - Microsoft Account Tenant ID
+- RAPYD_URL - Rapyd's sandbox URL
+- SITE_ID - SharePoint Site ID you wish to connect to
+
+## Run Tests
+#### Note: Set the Rapyd environment variable before running the tests
+
+```
+python -m unittest discover
+```
+
+## Architecture Diagram
+![Architecture Diagram](./PowerAppsImages/architecture-diagram.png)
+
+
+## Dashboard - Dark
+![Dashboard](./PowerAppsImages/dashboard-dark.png)
+
--- powerPayCheckout/tests/test_helpers.py
+++ powerPayCheckout/tests/test_helpers.py
@@ -1,7 +1,11 @@
 import unittest
 from unittest.mock import MagicMock
 
-from powerPayCheckout.helpers import generate_invoice_number, get_params
+from powerPayCheckout.helpers import (
+    generate_invoice_number,
+    get_params,
+    get_request_header,
+)
 
 
 class HelperTest(unittest.TestCase):
@@ -11,6 +15,8 @@ class HelperTest(unittest.TestCase):
         self.mock_request = self.general_mock.mock_request()
 
     def test_get_params_with_the_params(self):
+        """Test if we have the value checkout for type params"""
+
         test_params, expected_result = "type", "checkout"
 
         self.mock_request.params.get.return_value = "checkout"
@@ -22,6 +28,8 @@ class HelperTest(unittest.TestCase):
         self.assertEqual(test_result, expected_result)
 
     def test_get_params_without_the_params(self):
+        """Test if we have id params in the request body"""
+
         test_params = "id"
 
         self.mock_request.params.get.return_value = None
@@ -33,9 +41,27 @@ class HelperTest(unittest.TestCase):
             get_params(test_params, self.mock_request)
 
     def test_generate_invoice_number(self):
+        """Test if we have the requested number of characters in the invoice number"""
 
         test_result = generate_invoice_number()
 
         self.assertIsInstance(test_result, str)
 
         self.assertEqual(test_result.__len__(), 8)
+
+    def test_get_request_header_if_header_is_generated(self):
+        """Test if we have generate the perfect header"""
+
+        test_path, test_body = "/api/checkout", "{'amount':'230.49'}"
+        test_result = get_request_header(test_path, test_body)
+        header_keys = ["access_key", "signature", "salt", "timestamp", "Content-Type"]
+
+        test_result_keys = test_result.keys()
+
+        self.assertIsInstance(test_result, dict)
+
+        self.assertEqual(test_result.keys().__len__(), 5)
+
+        self.assertTrue(
+            all([True if keys in test_result_keys else False for keys in header_keys])
+        )
